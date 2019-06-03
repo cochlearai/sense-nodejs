336605c1f3527cb4bf17ef9826be4b1b69160e66
Benjamin Chabanne <benjamin.chabanne.95@gmail.com>
2019-06-03 22:24:05 +0900

    Add float support

--- src/SamplingFormat.ts
+++ src/SamplingFormat.ts
@@ -2,19 +2,27 @@ export enum SamplingFormat {
     Int64Bit,
     Int32Bit,
     Int16Bit,
-    Int8Bit
+    Int8Bit,
+    Float64Bit,
+    Float32Bit,
+    Float16Bit,
+    Float8Bit
 }
 
 export namespace SamplingFormat {
     export function samplingFormatToByteCount(samplingFormat: SamplingFormat): number {
         switch(samplingFormat){
             case SamplingFormat.Int8Bit:
+            case SamplingFormat.Float8Bit:
                 return 1;
             case SamplingFormat.Int16Bit:
+            case SamplingFormat.Float16Bit:
                     return 2;
             case SamplingFormat.Int32Bit:
+            case SamplingFormat.Float32Bit:
                     return 4;
             case SamplingFormat.Int64Bit:
+            case SamplingFormat.Float64Bit:
                     return 8;
             default:
                 throw new Error("Sampling format not recognized");
@@ -28,6 +36,11 @@ export namespace SamplingFormat {
             case SamplingFormat.Int32Bit:
             case SamplingFormat.Int64Bit:
                     return "int";
+            case SamplingFormat.Float8Bit:
+            case SamplingFormat.Float16Bit:
+            case SamplingFormat.Float32Bit:
+            case SamplingFormat.Float64Bit:
+                    return "float";
             default:
                 throw new Error("Sampling format not recognized");
         }
--- src/__tests__/SamplingFormat.test.ts
+++ src/__tests__/SamplingFormat.test.ts
@@ -24,4 +24,29 @@ describe("SamplingFormat", () => {
         expect(SamplingFormat.samplingFormatToByteCount(sf)).toBe(8);
         expect(SamplingFormat.samplingFormatToNumberType(sf)).toBe("int");
     });
+
+    test("Test float 8bit", () => {
+        const sf = SamplingFormat.Float8Bit;
+        expect(SamplingFormat.samplingFormatToByteCount(sf)).toBe(1);
+        expect(SamplingFormat.samplingFormatToNumberType(sf)).toBe("float");
+    });
+
+    test("Test float 16bit", () => {
+        const sf = SamplingFormat.Float16Bit;
+        expect(SamplingFormat.samplingFormatToByteCount(sf)).toBe(2);
+        expect(SamplingFormat.samplingFormatToNumberType(sf)).toBe("float");
+    });
+
+    test("Test float 32bit", () => {
+        const sf = SamplingFormat.Float32Bit;
+        expect(SamplingFormat.samplingFormatToByteCount(sf)).toBe(4);
+        expect(SamplingFormat.samplingFormatToNumberType(sf)).toBe("float");
+    });
+
+    test("Test float 64bit", () => {
+        const sf = SamplingFormat.Float64Bit;
+        expect(SamplingFormat.samplingFormatToByteCount(sf)).toBe(8);
+        expect(SamplingFormat.samplingFormatToNumberType(sf)).toBe("float");
+    });
+
 })
